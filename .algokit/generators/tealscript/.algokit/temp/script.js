a1226b21340d68325e17ce66f85202cb68f8d3de
Joe Polny <joepolny@gmail.com>
2024-02-04 19:06:24 -0500

    add tsconfig

--- .algokit/generators/tealscript/.algokit/temp/script.js
+++ .algokit/generators/tealscript/.algokit/temp/script.js
@@ -6,11 +6,14 @@ const pkgJsonPath = path.join(contractsDir, 'package.json');
 const prettierConfigPath = path.join(contractsDir, '.prettierrc.toml');
 const eslintConfigPath = path.join(contractsDir, '.eslintrc.js');
 const jestConfigPath = path.join(contractsDir, 'jest.config.js');
+const tsConfigPath = path.join(contractsDir, 'tsconfig.json');
 
-const files = [pkgJsonPath, prettierConfigPath, eslintConfigPath, jestConfigPath];
+const files = [pkgJsonPath, prettierConfigPath, eslintConfigPath, jestConfigPath, tsConfigPath];
 
 files.forEach((file) => {
   if (!fs.existsSync(file)) {
     fs.copyFileSync(path.join(__dirname, path.basename(file)), file);
   }
 });
+
+fs.rmSync(__dirname, { recursive: true, force: true });
